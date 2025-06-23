8a8c14c07894b31695fb547b816fd9b62a0d0012
DESKTOP-LJQ78DH\의석 <dkdlelxoa@naver.com>
2025-06-23 22:08:07 +0900

    ✨feat: HiMatch rag 초기 설정

---
+++ src/main/java/kr/co/himatch/ai/PdfragApplication.java
@@ -0,0 +1,12 @@
+package kr.co.himatch.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfragApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfragApplication.class, args);
+	}
+}
